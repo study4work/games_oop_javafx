69775eda1f56f930ade99f3fae63c45572370343
study4work <FaJlJleN@ya.ru>
2020-10-22 20:54:54 +0300

    2. Сокобан. Тестовое задание.

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -28,6 +28,7 @@ public class Win {
         for (int i = 0; i < board.length; i++) {
             if (board[i][i] == 1) {
                 rsl = verticalCheck(board, i) || horizontalCheck(board, i);
+                break;
             }
         }
 
